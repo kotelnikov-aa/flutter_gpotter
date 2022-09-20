8906bd447b463c0a7654b07c62c9c1eaf6722d87
chevic4 <victor.chinenov@gmail.com>
2022-09-20 10:04:20 +0300

    навигация и авторизация

--- lib/presentation/pages/tabscreen.dart
+++ lib/presentation/pages/tabscreen.dart
@@ -1,27 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_gpotter/presentation/pages/welcome_screen.dart';
-import 'package:flutter_gpotter/presentation/pages/card_screen.dart';
-import 'package:flutter_gpotter/presentation/pages/favorite_list_scren.dart';
+import 'package:flutter_gpotter/internal/navigation/navigation.dart';
 
 late User loggedinUser;
 
 class TabScreen extends StatefulWidget {
   const TabScreen({Key? key}) : super(key: key);
 
-
   @override
   State<TabScreen> createState() => _TabScreenState();
 }
 
 class _TabScreenState extends State<TabScreen> {
-  int _currentIndex = 2;
-
+  int _currentIndex = 0;
+  bool _favorite = true;
+  NavigatorsPages navigatorsPages = NavigatorsPages();
   final _auth = FirebaseAuth.instance;
 
   void initState() {
     super.initState();
     getCurrentUser();
+  }
+
+  void changeTab(int index) {
+    setState(() {
+      _currentIndex = index;
+      navigatorsPages.setcurrentpagekey = NavigatorsPages.keyspages[index];
+    });
+  }
+
+  void changeFavorite() {
+    setState(() {
+      navigatorsPages.setcurrentpagekey =
+          NavigatorsPages.keyspages[_currentIndex];
+
+      if (_favorite == false) {
+        _favorite = true;
+      } else {
+        _favorite = false;
+      }
+    });
   }
 
   //using this function you can use the credentials of the user
@@ -39,36 +57,59 @@ class _TabScreenState extends State<TabScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('d')),
-      body: IndexedStack(
-        index: _currentIndex,
-        children:  [
-          WelcomeScreen(),
-          const CardScreen(),
-          const FavoriteListScreen(),
-
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(_favorite ? 'favorite screen' : 'main screen'),
+        backgroundColor: (_favorite ? Colors.red : Colors.grey),
+        actions: [
+          TextButton(
+              onPressed: (() {
+                changeFavorite();
+              }),
+              child: Icon(Icons.star,
+                  color: (_favorite ? Colors.amber : Colors.black))),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(
-          label: 'abc',
-          icon: Icon(
-            Icons.abc,
-          ),
-        ),
-        BottomNavigationBarItem(
-          label: 'abc',
-          icon: Icon(
-            Icons.abc,
-          ),
-        ),
-        BottomNavigationBarItem(
-          label: 'abc',
-          icon: Icon(
-            Icons.abc,
-          ),
-        ),
-      ]),
+      body: _favorite
+          ? IndexedStack(
+              index: _currentIndex,
+              children: navigatorsPages.favoritelistpages,
+            )
+          : IndexedStack(
+              index: _currentIndex,
+              children: navigatorsPages.listpages,
+            ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: changeTab,
+          backgroundColor: (_favorite ? Colors.red : Colors.grey),
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Elixirs',
+              icon: Icon(
+                Icons.drag_indicator_sharp,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: 'Houses',
+              icon: Icon(
+                Icons.home,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: 'Spells',
+              icon: Icon(
+                Icons.abc,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: 'Wizards',
+              icon: Icon(
+                Icons.wallet,
+              ),
+            ),
+          ]),
     );
   }
 }
