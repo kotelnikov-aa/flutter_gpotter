c97a306dff9159c1d8202800d8daf7b2dfb38761
kotelnikov-aa <101976294+kotelnikov-aa@users.noreply.github.com>
2022-11-03 22:55:10 +0700

    добавлена авторизация по биометрии

--- lib/presentation/auth_pages/welcome_screen.dart
+++ lib/presentation/auth_pages/welcome_screen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gpotter/internal/constants/colors.dart';
 import 'package:flutter_gpotter/presentation/pages/widgets_pages/potter_button.dart';
+import 'package:local_auth/local_auth.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
+
+
+  Future<void> _authenticateWithBiometrics(BuildContext context) async {
+    bool authenticated = false;
+    final LocalAuthentication auth = LocalAuthentication();
+    authenticated = await auth.authenticate(
+        localizedReason:
+        'Scan your fingerprint (or face or whatever) to authenticate',
+        options: const AuthenticationOptions(
+          stickyAuth: true,
+          biometricOnly: true,
+        ),
+      );
+    if (authenticated) {
+      Navigator.pushNamed(context, 'home_screen');
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +51,11 @@ class WelcomeScreen extends StatelessWidget {
                 child: const BottonPotter(value: 'registration'),
                 onTap: () {
                   Navigator.pushNamed(context, 'registration_screen');
+                },
+              ),GestureDetector(
+                child: const BottonPotter(value: 'biometric'),
+                onTap: () {
+                  _authenticateWithBiometrics(context);
                 },
               ),
             ],
